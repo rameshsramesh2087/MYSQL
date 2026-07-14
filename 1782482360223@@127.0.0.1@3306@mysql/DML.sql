7342dd67304e3dc14828a9d9f89e4c6da511454b
ramesh <rameshsramesh2087@gmail.com>
2026-07-14 10:40:30 +0530

    ddl,dml

---
+++ 1782482360223@@127.0.0.1@3306@mysql/DML.sql
@@ -0,0 +1,18 @@
+INSERT INTO CUSTOMER(
+    CUSTOMER_ID,
+    CUS_NAME,
+    CUS_EMAIL,
+    CUS_SALARY,
+    CUS_LOCATION) 
+VALUES (1,'RAMESH','ramesh@gmail.com',50000,'chennai');
+select * from customer ;
+
+INSERT INTO CUSTOMER VALUES(2,'RAJKUMAR','rajkumar@gmail.com',60000,'BANGALORE'),
+(3,'KUMARA VEL','kumaravel@gmail.com',55000,'COIMBATORE'),
+(4,'SHIVASHANKAR','shivasankar@gmail.com',65000,'MUMBAI'),
+(5,'MITHUN KUMAR','mithun@gmail.com',70000,'DELHI'),
+(6,'RAJESH KUMAR','rajesh@gmail.com',80000,'HYDERABAD'),
+(7,'KUMARESHAN','kumareshan@gmail.com',85000,'PUNE'),
+(8,'SHINIVASAN','shinivasan@gmail.com',90000,'CHENNAI'),
+(9,'VASANTH KUMAR','vasanth@gmail.com',100000,'BANGALORE'),
+(10,'DIVAKARAN','divakaran@gmail.com',120000,'COIMBATORE');
